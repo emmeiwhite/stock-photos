a8d5fae7a49670f0dc734a62ad91331b37adf3c2
Imran Rafiq Rather <emmeiwhite@gmail.com>
2021-01-30 01:13:39 +0530

    Search Form ready

--- src/App.js
+++ src/App.js
@@ -2,29 +2,57 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 
-// const KEY = "chfO2I4YSDqNBSVZBb0DbYdEDluUb-gPUVSG-cqnEBc";
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-
 const clientID = `?client_id=${process.env.REACT_APP_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState(false);
+
   const getPhotos = async () => {
+    setLoading(true);
     let url = `${mainUrl}${clientID}`; // Setting the environmental variable
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      setPhotos(data);
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
+      setError(true);
       console.log(error);
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   useEffect(() => {
     getPhotos();
   }, []);
-  return <h2>stock photos starter</h2>;
+
+  if (loading) {
+    return <h4>loading...</h4>;
+  }
+
+  if (error) {
+    return <h3>ERROR: Something went Wrong </h3>;
+  }
+  return (
+    <main>
+      <section className="search">
+        <form onSubmit={handleSubmit} className="search-form">
+          <input type="text" className="form-input " />
+          <button type="submit" onClick={handleSubmit} className="submit-btn">
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default App;
